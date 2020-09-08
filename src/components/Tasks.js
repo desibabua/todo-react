ec240d1f95983ec6b04d0d17c0fe894a7d795929
ayush <58028488+desibabua@users.noreply.github.com>
2020-09-08 21:40:25 +0530

    refactored tasks function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,15 +2,8 @@ import React from 'react';
 import Task from './Task';
 
 const Tasks = function ({ toDos, onClick, onDelete }) {
-  return toDos.map(({ id, task, status }) => (
-    <Task
-      key={id}
-      id={id}
-      task={task}
-      status={status}
-      onClick={onClick}
-      onDelete={onDelete}
-    />
+  return toDos.map((toDo) => (
+    <Task key={toDo.id} {...toDo} onClick={onClick} onDelete={onDelete} />
   ));
 };
 
