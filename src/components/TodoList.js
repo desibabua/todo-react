a3cad7390559a1612f518ae9198faba8d4cf178a
ayush <58028488+desibabua@users.noreply.github.com>
2020-09-08 21:57:43 +0530

    added hooks to todoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,73 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './Title';
 import Tasks from './Tasks';
 import AddTask from './InputBox';
 import { getDefaultStatus, getNextStatus } from './Statuses';
 
-const DEFAULT_HEADING = 'Todo'
+const DEFAULT_HEADING = 'Todo';
 
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { title: DEFAULT_HEADING, toDos: [], lastTaskId: 0 };
-    this.resetTodo = this.resetTodo.bind(this);
-    this.updateTitle = this.updateTitle.bind(this);
-    this.addTask = this.addTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
-    this.updateStatus = this.updateStatus.bind(this);
-  }
+const TodoList = function () {
+  const [title, setTitle] = useState(DEFAULT_HEADING);
+  const [toDos, setToDos] = useState([]);
+  const [lastTaskId, setLastTaskId] = useState(0);
 
-  resetTodo() {
-    this.setState(() => ({ title: DEFAULT_HEADING, toDos: [], lastTaskId: 0 }));
-  }
+  const resetTodo = function () {
+    setTitle(DEFAULT_HEADING);
+    setToDos([]);
+    setLastTaskId(0);
+  };
 
-  updateTitle(title) {
-    this.setState(() => ({ title }));
-  }
+  const updateTitle = function (title) {
+    setTitle(title);
+  };
 
-  addTask(task) {
-    this.setState(({ toDos, lastTaskId }) => ({
-      toDos: toDos.concat({
-        id: lastTaskId + 1,
-        task,
-        status: getDefaultStatus(),
-      }),
-      lastTaskId: lastTaskId + 1,
-    }));
-  }
+  const addTask = function (task) {
+    setToDos([...toDos, { id: lastTaskId, task, status: getDefaultStatus() }]);
+    setLastTaskId(lastTaskId + 1);
+  };
 
-  deleteTask(taskId) {
-    this.setState(({ toDos }) => ({
-      toDos: toDos.filter((task) => task.id !== taskId),
-    }));
-  }
+  const deleteTask = function (taskId) {
+    setToDos(toDos.filter((task) => task.id !== taskId));
+  };
 
-  updateStatus(taskId) {
-    this.setState((state) => {
-      const toDos = state.toDos.map((task) => ({ ...task }));
-      const task = toDos.find((task) => task.id === taskId);
-      task.status = getNextStatus(task.status);
-      return { toDos };
-    });
-  }
+  const updateStatus = function (taskId) {
+    const task = toDos.find((task) => task.id === taskId);
+    task.status = getNextStatus(task.status);
+    setToDos([...toDos]);
+  };
 
-  render() {
-    return (
-      <div>
-        <Title
-          title={this.state.title}
-          onSubmit={this.updateTitle}
-          onDelete={this.resetTodo}
-        />
-        <Tasks
-          toDos={this.state.toDos}
-          onClick={this.updateStatus}
-          onDelete={this.deleteTask}
-        />
-        <AddTask onSubmit={this.addTask} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Title title={title} onSubmit={updateTitle} onDelete={resetTodo} />
+      <Tasks toDos={toDos} onClick={updateStatus} onDelete={deleteTask} />
+      <AddTask onSubmit={addTask} />
+    </div>
+  );
+};
 
 export default TodoList;
